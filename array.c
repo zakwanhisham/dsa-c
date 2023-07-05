fd16efe0724e82bf5d9262b95d1ea77485b05f8f
zakwan hisham <hishamzakwan@gmail.com>
2023-07-05 17:14:35 +0800

    Array

---
+++ array.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+static void display(int intArray[], int length) {
+    int i = 0;
+    printf("Array : [");
+
+    for (i = 0; i < length; ++i) {
+        printf(" %d ", intArray[i]);
+    }
+    printf(" ]\n");
+}
+
+int main() {
+    int i = 0;
+    int intArray[8];
+
+    for (i = 0; i < 8; ++i) {
+        intArray[i] = 0;
+    }
+    printf("Array with default data.");
+
+    /* Display elements of an array */
+    display(intArray, 8);
+
+    /* Operation:Insertion
+     * Add elements in the array */
+    for (i = 0; i < 8; ++i) {
+        printf("Adding %d at index %d\n", i, i);
+        intArray[i] = i;
+    }
+    printf("\n");
+    printf("Array after adding data. ");
+    display(intArray, 8);
+
+    /*Operation:Insertion
+     * Element at any location can be updated directly*/
+    int index = 5;
+    intArray[index] = 10;
+
+    printf("Array after updating element at index %d.\n", index);
+    display(intArray, 8);
+
+    /* Operation:Search using index
+     * Search an element using index*/
+    printf("Data at index %d: %d\n", index, intArray[index]);
+
+    /* Operation:Search using value
+     * Search an element using value*/
+    int value = 4;
+    for (i = 0; i < 8; ++i) {
+        if (intArray[i] == value) {
+            printf("value %d Found at index %d \n", intArray[i], i);
+            break;
+        }
+    }
+
+    return 0;
+}
